0f546178920e7ece702d38691da2aa06d4d19f6a
Girish S <girishs@Girishs-MacBook-Pro.local>
2025-06-05 16:06:12 +0530

    Initialised

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 App running at http://localhost:${PORT}`);
+});
